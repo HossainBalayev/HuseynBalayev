ecdb72dc5fe29175f0545fe1c3543ca2846b5084
HossainBalayev <balayev.huseyn@bk.ru>
2024-08-05 19:34:58 +0400

    formlar

--- models/forms.py
+++ models/forms.py
@@ -11,3 +11,21 @@ class ContactForm(forms.ModelForm):
             'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Your Email'}),
             'message': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Your Message'}),
         }
+
+    def clean_name(self):
+        name = self.cleaned_data['name']
+        if len(name) < 2:
+            raise forms.ValidationError('Name must be at least 2 characters long')
+        return name
+    
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if Contact.objects.filter(email=email).exists():
+            raise forms.ValidationError('This mail is already in use')
+        return email
+    
+
+    def clean_message(self, message):
+        if len(message) < 10:
+            raise forms.ValidationError('Message must be at least 10 characters long')
+        return message
